a39a0ceea9115b66bb1603f8f333e97a26e7a0c1
nayukiH <nayukicom@sina.cn>
2019-03-07 17:51:38 +0800

    some word

---
+++ homework2.1/homework2.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_one
+{
+    class HelloWorld
+    {
+        public void output(String[] args)
+        {
+            Console.WriteLine("Homework NO.1:");
+            if (args.Length >= 2)
+            {
+                Console.WriteLine("Hello World! From {0} To {1}", args[0], args[1]);
+                foreach (string i in args)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hello World! command line parameter does not meet the requirement!");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld myHello = new HelloWorld();
+            myHello.output(args);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ homework2.2/homework2.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2._2
+{
+    class HelloWorld
+    {
+        public static void output(String[] args)
+        {
+            Console.WriteLine("Homework NO.2:");
+            if (args.Length >= 2)
+            {
+                Console.WriteLine("Hello World! From {0} To {1}", args[0], args[1]);
+                foreach (string i in args)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hello World! command line parameter does not meet the requirement!");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.output(args);
+            Console.ReadLine();
+        }
+    }
+}
